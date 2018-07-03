f66a3ea1daf9c38bde52a360600bf78f189392d7
James Babineau <jbabineau@homesite.com>
2018-07-03 08:31:41 -0400

    working copy of the nav and the index page.

--- src/main/java/com/jbabineau/controllers/IndexController.java
+++ src/main/java/com/jbabineau/controllers/IndexController.java
@@ -13,6 +13,14 @@ public class IndexController {
 
         model.addAttribute("appName", "TWSS board!");
         model.addAttribute("classActiveHome", "active");
+        model.addAttribute("header1", "index.header1");
+        model.addAttribute("header2", "index.header2");
+        model.addAttribute("header3", "index.header3");
+        model.addAttribute("header4", "index.header4");
+        model.addAttribute("text1", "index.text1");
+        model.addAttribute("text2", "index.text2");
+        model.addAttribute("text3", "index.text3");
+        model.addAttribute("text4", "index.text4");
         return "index";
     }
 }
